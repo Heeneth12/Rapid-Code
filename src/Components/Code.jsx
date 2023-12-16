85ea8d0448c8983b22daf720a012650347052b1e
Heeneth sai <114326514+Heeneth12@users.noreply.github.com>
2023-12-16 21:42:22 +0530

    Update Code.jsx
    input text added

--- src/Components/Code.jsx
+++ src/Components/Code.jsx
@@ -161,7 +161,7 @@ const Code = () => {
             <h1 style={{ fontFamily: "bold", fontSize: "large" }}>Input</h1>
             <textarea
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Write your thoughts here..."
+              placeholder="Input here..."
               name=""
               id=""
               cols="50"
